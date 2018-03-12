93244046a0fd60aba5dd536c0516363f325a0c8c
Steve Mandes <sa_web@echo.rutgers.edu>
2018-03-11 20:53:41 -0400

    Add files initial upload

---
+++ index.php
@@ -0,0 +1,179 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="https://www.rutgers.edu/sites/all/themes/uwide/favicon.ico" type="image/vnd.microsoft.icon">
+
+    <title>Rutgers University | Division of Student Affairs</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="<?php bloginfo(template_directory); ?>/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container">
+            <img class="img-fluid" src="<?php bloginfo(template_directory); ?>/images/sarutgers-shield-logo.png" alt="Rutgers Student Affairs">
+      </div><!-- /container -->
+    </nav>
+
+    <main role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="hero-banner space">
+        <img class="img-fluid hero-img" src="<?php bloginfo(template_directory); ?>/images/theres-a-u-bg.jpg" alt="Rutgers Student Affairs">
+      </div>
+
+      <div class="container">
+        <div class="row space">
+          <div class="col-md-12">
+            <h1>Excuse Our Appearance</h1>
+            <p>The website you may be looking for is currently undergoing maintenance and will be back shortly. In the meantime, have a look at the current available resources that the Division of Student Affairs has to offer.</p>
+
+            <p>If you have any questions, please contact the appropriate number and a representative in that department can assist you.</p>
+          </div><!-- /col -->
+        </div><!-- /row -->
+
+
+
+
+
+        <div class="row space">
+          <div class="col-md-6">
+            <h2>Counseling, ADAP and Psychiatric Services (CAPS)</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-7884<br>
+              <i class="fas fa-phone-square"></i> <strong>National Suicide Prevention Lifeline:</strong> 800-274-TALK (8255)<br>
+              <i class="fas fa-phone-square"></i> <strong>NJ Hope Line:</strong> 855-654-6735<br>
+              <i class="fas fa-phone-square"></i> <strong>National Crisis Text Line:</strong> Text KNIGHTS to 741741; If you are a person of color you can also text STEVE to 741741.<br>
+            </p>
+            <p>When CAPS is closed, Acute Psychiatric Service provides 24-hour emergency service for local residents. 855-515-5700. If you live outside of Middlesex county you should contact the nearest hospital emergency department.</p>
+
+
+            <h2>Health Outreach, Promotion and Education (HOPE)</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-1965</p>
+
+
+            <h2>Violence Prevention and Victim Assistance</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-1181</p>
+
+
+            <h2>Pharmacy Services</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-8033</p>
+
+
+            <h2>Student Health</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-9063</p>
+
+
+            <h2>Dean of Students</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-2300</p>
+
+
+            <h2>Dining Services</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-8041 <br><i class="fas fa-phone-square"></i> 848-932-8469</p>
+
+
+            <h2>Student Centers</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-8070</p>
+
+
+            <h2>Recreational Services</h2>
+            <p><i class="fas fa-phone-square"></i> 848-445-0460</p>
+
+
+            <h2>Student Legal Services</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-4529</p>
+
+
+            <h2>Major Events and Programs</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-7213</p>
+
+
+            <h2>Fraternity and Sorority Affairs</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-7692</p>
+
+          </div><!-- /col -->
+
+          <div class="col-md-6">
+            <h2>Graduate Student Life</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-0614</p>
+
+
+            <h2>Center for Social Justice Education and LGBT Communities</h2>
+            <p><i class="fas fa-phone-square"></i> 848-445-4141</p>
+
+
+            <h2>Center for Latino Arts and Culture (CLAC)</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-1263</p>
+
+
+            <h2>Student Activities Business Office</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-6981</p>
+
+
+            <h2>Leadership and Experiential Learning</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-4086</p>
+
+
+            <h2>Off-Campus Living and Community Partnerships</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-5500</p>
+
+
+            <h2>Paul Robeson Cultural Center (PRCC)</h2>
+            <p><i class="fas fa-phone-square"></i>  848-445-3545</p>
+
+
+            <h2>Student Affairs Compliance</h2>
+            <p><i class="fas fa-phone-square"></i>  848-932-1181</p>
+
+
+
+            <h2>Student Involvement</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-6978</p>
+
+
+
+            <h2>Student Conduct</h2>
+            <p><i class="fas fa-phone-square"></i> 848-932-9414</p>
+
+
+            <h2>Asian American Cultural Center (AACC)</h2>
+            <p><i class="fas fa-phone-square"></i>  848-445-8043</p>
+
+          </div><!-- /col -->
+        </div><!-- /row -->
+
+      </div> <!-- /container -->
+
+    </main>
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10">
+            <p class="copyright">Copyright &copy;  2018, Rutgers, The State University of New Jersey,
+  an equal opportunity, affirmative action institution. All rights reserved.</p>
+          </div><!-- /col -->
+
+          <div class="col-md-2">
+            <a href="http://search.rutgers.edu/" target="_blank">Search Rutgers</a>
+          </div><!-- /col -->
+        </div><!-- /row -->
+      </div><!-- /container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+  </body>
+</html>
